c526ae3b9d439e2ff8dec67f03263a93fbef36bf
Renata Macedo <macedorenata.developer@gmail.com>
2022-05-25 23:40:36 -0300

    creating header form

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -7,6 +7,10 @@ import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import { styles } from "./styles";
 import { theme } from "../../theme";
 import { Options } from "../Options";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { Form } from "../Form";
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -27,7 +31,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form feedbackType="BUG" />
       </BottomSheet>
     </>
   );
